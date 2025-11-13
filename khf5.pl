2c46e4d0dd8c4ca7b4e17ca85f4ae60a31fd7c1e
Petya-G <vegvaripeter21@gmail.com>
2025-11-13 18:37:31 +0100

    still working

--- khf5.pl
+++ khf5.pl
@@ -14,15 +14,20 @@
 
 % :- pred ismert_szukites(feladvany_leiro::in, t_matrix::in, t_matrix::out).
 
+%ismert_szukites(szt(N, M, _), Mx0, Mx).
+    
 kezdotabla(szt(N, M, []), Mx) :-
     fill(N, M, Mx).
 
-kezdotabla(szt(N, M, FLeiro), Mx) :-
+kezdotabla(szt(N, M, L), Mx) :-
     fill(N, M, Mx0),
-    foldl(replace_entry, FLeiro, Mx0, Mx).
+    foldl(replace_entry, L, Mx0, Mx),
+    print(Mx).
 
 replace_entry(i(J,K,E), Matrix, NewMatrix) :-
-    replace_m_n(Matrix, J, K, E, NewMatrix).
+    J1 is J - 1,
+    K1 is K - 1,
+    replace_Mx(Matrix, J1, K1, _, E, NewMatrix).
 
 fill(N, M, Mx) :-
     length(Mx, N),
@@ -37,13 +42,11 @@ fill_rows(N, M, [H|T], TT) :-
     maplist(=(TT), H),
     fill_rows(N, M, T, TT).
 
-replace_m_n(Matrix, I, J, NewValue, NewMatrix) :-
-    I1 is I - 1,
-    J1 is J - 1,
-    replace_nth(I1, Old1, New1, Matrix, NewMatrix),
-    replace_nth(J1, _OldElem2, NewValue, Old1, New1).
+replace_Mx(Matrix, I, J, OldValue, NewValue, NewMatrix) :-
+    replace_L(I, Old1, New1, Matrix, NewMatrix),
+    replace_L(J, OldValue, NewValue, Old1, New1).
 
-replace_nth(N, OldElem, NewElem, List, List2) :-
+replace_L(N, OldElem, NewElem, List, List2) :-
     length(L1, N),
     append(L1, [OldElem|Rest], List),
     append(L1, [NewElem|Rest], List2).
